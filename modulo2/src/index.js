4e6616613ef97ddf548854f3d4cf1fd009ec4164
Cassio <cassiojr0108@gmail.com>
2023-11-21 11:20:39 -0300

    metodos http

--- modulo2/src/index.js
+++ modulo2/src/index.js
@@ -3,10 +3,34 @@ const express = require('express');
 const app = express();
 const port = 3000
 
-app.get('/', function(req, res) {
-    return res.json({
-        message: 'Olá Dev! Bem vindo ao curso!'
-    });
+app.get('/projects', function(req, res) {
+    return res.json([
+        'Projeto 1', 
+        'Projeto 2'
+    ]);
+});
+
+app.post('/projects', function(req, res) {
+    return res.json([
+        'Projeto 1', 
+        'Projeto 2',
+        'Projeto 3'
+    ]);
+});
+
+app.put('/projects/:id', function(req, res) {
+    return res.json([
+        'Projeto 4', 
+        'Projeto 2',
+        'Projeto 3'
+    ]);
+});
+
+app.delete('/projects/:id', function(req, res) {
+    return res.json([
+        'Projeto 2',
+        'Projeto 3'
+    ]);
 });
 
 app.listen(port, () => console.log('Server started port ' + port));
