fa93884570250551cc0bdfe6f94d04f84571d853
Cassio <cassiojr0108@gmail.com>
2023-11-23 15:14:32 -0300

    tratamento de erros

--- modulo3/src/shared/http/routes/index.ts
+++ modulo3/src/shared/http/routes/index.ts
@@ -1,10 +1,11 @@
 import { Router, Request, Response } from "express"
+import { AppError } from "src/shared/errors/AppError"
 
 const routes = Router()
 
 routes.get('/', (req: Request, res: Response) => {
+    throw new AppError('Acesso negado', 401)
     return res.json({message: 'Olá Dev!'})
 })
-
 
 export { routes }
--- modulo3/src/shared/http/server.ts
+++ modulo3/src/shared/http/server.ts
@@ -1,14 +1,33 @@
 import 'dotenv/config' //variaveis de ambiente
-import express, {Request, Response} from "express"
+import express, { NextFunction, Request, Response } from "express"
 import 'express-async-errors'
 import cors from 'cors'
 import { routes } from './routes'
+import { AppError } from '../errors/AppError'
 
 const app = express()
 app.use(cors())
 app.use(express.json())
 
 app.use(routes)
+
+app.use((
+    error: Error,
+    req: Request,
+    res: Response,
+    next: NextFunction)=>{
+        if(error instanceof AppError) {
+            return res.status(error.statusCode).json({
+                status: 'error',
+                message: error.message,
+            })
+        }
+        console.log(error)
+        return res.status(500).json({
+            status: 'error',
+            message: 'Internal server error',
+        })
+})
 
 app.listen(process.env.PORT, () => {
     console.log(`Server started on port ${process.env.PORT}`)
