dc59d32c5635ea7f7a9c0c52778f321658785899
Cassio <cassiojr0108@gmail.com>
2023-11-23 14:26:16 -0300

    routes

--- modulo3/src/shared/http/server.ts
+++ modulo3/src/shared/http/server.ts
@@ -2,14 +2,13 @@ import 'dotenv/config' //variaveis de ambiente
 import express, {Request, Response} from "express"
 import 'express-async-errors'
 import cors from 'cors'
+import { routes } from './routes'
 
 const app = express()
 app.use(cors())
 app.use(express.json())
 
-app.get('/', (req: Request, res: Response) => {
-    return res.json({message: 'Olá Dev!'})
-})
+app.use(routes)
 
 app.listen(process.env.PORT, () => {
     console.log(`Server started on port ${process.env.PORT}`)
