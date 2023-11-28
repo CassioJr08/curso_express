8408ec2d0fd7dcc931796200cfbf80e14c712d5e
Cassio <cassiojr0108@gmail.com>
2023-11-28 16:41:51 -0300

    .

--- modulo3/src/shared/http/server.ts
+++ modulo3/src/shared/http/server.ts
@@ -1,39 +1,13 @@
 import 'dotenv/config' //variaveis de ambiente
-import express, { NextFunction, Request, Response } from "express"
-import 'express-async-errors'
-import swaggerUi from 'swagger-ui-express'
-import cors from 'cors'
-import { routes } from './routes'
-import { AppError } from '../errors/AppError'
-import swaggerFile from '../../swagger.json'
+import 'reflect-metadata'
+import { app } from './app'
+import { dataSource } from '../typeorm'
 
-const app = express()
-app.use(cors())
-app.use(express.json())
-app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerFile))
-
-app.use(routes)
-
-app.use((
-    error: Error,
-    req: Request,
-    res: Response,
-    next: NextFunction)=>{
-        if(error instanceof AppError) {
-            return res.status(error.statusCode).json({
-                status: 'error',
-                message: error.message,
-            })
-        }
-        console.log(error)
-        return res.status(500).json({
-            status: 'error',
-            message: 'Internal server error',
-        })
-})
-
-app.listen(process.env.PORT, () => {
-    console.log(`Server started on port ${process.env.PORT}`)
+dataSource.initialize().then(() => {
+    
+    app.listen(process.env.PORT, () => {
+        console.log(`Server started on port ${process.env.PORT}`)
+    })
 })
 
 
