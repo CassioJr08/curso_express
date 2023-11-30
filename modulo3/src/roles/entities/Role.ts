62e1a6a08b10e3e45337ce8550cfc9e87468c49a
Cassio <cassiojr0108@gmail.com>
2023-11-30 15:13:43 -0300

    create my table of roles

--- modulo3/src/roles/entities/Role.ts
+++ modulo3/src/roles/entities/Role.ts
@@ -1,4 +1,5 @@
-import { v4 as uuidv4 } from "uuid"
+import { v4 as uuidv4 } from "uuid"  
+//Em um contexto de desenvolvimento de backend usando Node.js, o termo “entidades” geralmente se refere a objetos ou modelos que representam os dados do seu aplicativo.
 
 export class Role {
     id?: string
--- modulo3/src/shared/typeorm/index.ts
+++ modulo3/src/shared/typeorm/index.ts
@@ -1,8 +1,9 @@
 import { DataSource } from "typeorm"
+import { CreateRolesTable1701366349104 } from "./migrations/1701366349104-CreateRolesTable"
 
 export const dataSource = new DataSource({
     type: 'sqlite',
     database: './db.sqlite',
     entities: [],
-    migrations: [],
+    migrations: [CreateRolesTable1701366349104],
 })
