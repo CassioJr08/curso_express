37a9eb0b3d9b7c58741258e9a080f94153f5942f
Cassio <cassiojr0108@gmail.com>
2023-11-30 15:29:29 -0300

    Refactor role entity

--- modulo3/src/roles/entities/Role.ts
+++ modulo3/src/roles/entities/Role.ts
@@ -1,9 +1,16 @@
+import { Column, CreateDateColumn, Entity, PrimaryColumn } from "typeorm"
 import { v4 as uuidv4 } from "uuid"  
 //Em um contexto de desenvolvimento de backend usando Node.js, o termo “entidades” geralmente se refere a objetos ou modelos que representam os dados do seu aplicativo.
 
+@Entity('roles')
 export class Role {
+    @PrimaryColumn()
     id?: string
+
+    @Column()
     name: string
+
+    @CreateDateColumn()
     created_at: Date
 
     constructor() {
--- modulo3/src/shared/typeorm/index.ts
+++ modulo3/src/shared/typeorm/index.ts
@@ -1,9 +1,10 @@
 import { DataSource } from "typeorm"
 import { CreateRolesTable1701366349104 } from "./migrations/1701366349104-CreateRolesTable"
+import { Role } from "@roles/entities/Role"
 
 export const dataSource = new DataSource({
     type: 'sqlite',
     database: './db.sqlite',
-    entities: [],
+    entities: [Role],
     migrations: [CreateRolesTable1701366349104],
 })
