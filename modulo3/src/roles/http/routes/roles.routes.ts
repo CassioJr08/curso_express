b09e95bfb5f9e4b6bae2d72b4f9eaf15b77e9181
Cassio <cassiojr0108@gmail.com>
2023-12-01 21:06:07 -0300

    create delete roule

--- modulo3/src/roles/http/routes/roles.routes.ts
+++ modulo3/src/roles/http/routes/roles.routes.ts
@@ -1,4 +1,5 @@
 import { createRolesController } from "@roles/useCases/createRole"
+import { deleteRolesController } from "@roles/useCases/deleteRole"
 import { listRolesController } from "@roles/useCases/listRoles"
 import { showRolesController } from "@roles/useCases/showRole"
 import { updateRolesController } from "@roles/useCases/updateRole"
@@ -24,8 +25,12 @@ rolesRoute.get('/:id', (req: Request, res: Response) =>{
 rolesRoute.put('/:id', (req: Request, res: Response) =>{
     return updateRolesController.handle(req, res)
 })
+
+rolesRoute.delete('/:id', (req: Request, res: Response) =>{
+    return deleteRolesController.handle(req, res)
+})
+
+
 export {rolesRoute}
-
-
 
 
