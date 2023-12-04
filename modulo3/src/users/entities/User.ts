06cefcc652f229742dda5e67d6c7a3fa5c96722b
Cassio <cassiojr0108@gmail.com>
2023-12-04 17:19:56 -0300

    Add CreateUseUseCase

--- modulo3/src/users/entities/User.ts
+++ modulo3/src/users/entities/User.ts
@@ -1,42 +1,39 @@
-import { Role } from "@roles/entities/Role"
-import { Column, CreateDateColumn, Entity, ManyToOne, PrimaryColumn } from "typeorm"
-import { v4 as uuidv4 } from "uuid"  
-// Em um contexto de desenvolvimento de backend
-// usando Node.js, o termo “entidades” geralmente
-// se refere a objetos ou modelos que representam
-// os dados do seu aplicativo.
+import { Role } from '@roles/entities/Role'
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  ManyToOne,
+  PrimaryColumn,
+} from 'typeorm'
+import { v4 as uuidv4 } from 'uuid'
 
 @Entity('users')
 export class User {
-    @PrimaryColumn()
-    id?: string
+  @PrimaryColumn()
+  id?: string
+  @Column()
+  name: string
+  @Column()
+  email: string
+  @Column()
+  password: string
+  @Column()
+  isAdmin: boolean
+  @Column()
+  avatar?: string
 
-    @Column()
-    name: string
+  @ManyToOne(() => Role, {
+    cascade: true,
+  })
+  role: Role
 
-    @Column()
-    email: string
+  @CreateDateColumn()
+  created_at: Date
 
-    @Column()
-    password: string
-
-    @Column()
-    isAdmin: boolean
-
-    @Column()
-    avatar?: string
-
-    @ManyToOne(() => Role, {
-        cascade: true,
-    })
-    role: Role
-
-    @CreateDateColumn()
-    created_at: Date
-
-    constructor() {
-        if(!this.id){
-            this.id = uuidv4()
-        }
+  constructor() {
+    if (!this.id) {
+      this.id = uuidv4()
     }
+  }
 }
