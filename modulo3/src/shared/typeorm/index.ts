fa7fcf3280e68f7fee9f777cb7db2862fff931c2
Cassio <cassiojr0108@gmail.com>
2023-12-04 14:41:41 -0300

    Create migration AddroleIdToUsersTable1701710657971

--- modulo3/src/shared/typeorm/index.ts
+++ modulo3/src/shared/typeorm/index.ts
@@ -2,10 +2,15 @@ import { DataSource } from "typeorm"
 import { CreateRolesTable1701366349104 } from "./migrations/1701366349104-CreateRolesTable"
 import { Role } from "@roles/entities/Role"
 import { CreateUsersTable1701709190656 } from "./migrations/1701709190656-CreateUsersTable"
+import { AddroleIdToUsersTable1701710657971 } from "./migrations/1701710657971-AddroleIdToUsersTable"
 
 export const dataSource = new DataSource({
     type: 'sqlite',
     database: './db.sqlite',
     entities: [Role],
-    migrations: [CreateRolesTable1701366349104, CreateUsersTable1701709190656],
+    migrations: [
+        CreateRolesTable1701366349104, 
+        CreateUsersTable1701709190656,
+        AddroleIdToUsersTable1701710657971
+    ]
 })
