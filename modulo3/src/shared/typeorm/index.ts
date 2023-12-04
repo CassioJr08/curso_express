00749ce2b4d9fe665f41c770e619374b26570a8a
Cassio <cassiojr0108@gmail.com>
2023-12-04 14:17:33 -0300

    create users table

--- modulo3/src/shared/typeorm/index.ts
+++ modulo3/src/shared/typeorm/index.ts
@@ -1,10 +1,11 @@
 import { DataSource } from "typeorm"
 import { CreateRolesTable1701366349104 } from "./migrations/1701366349104-CreateRolesTable"
 import { Role } from "@roles/entities/Role"
+import { CreateUsersTable1701709190656 } from "./migrations/1701709190656-CreateUsersTable"
 
 export const dataSource = new DataSource({
     type: 'sqlite',
     database: './db.sqlite',
     entities: [Role],
-    migrations: [CreateRolesTable1701366349104],
+    migrations: [CreateRolesTable1701366349104, CreateUsersTable1701709190656],
 })
