0c4908861c8908044cadb4c13f15989a12f2029e
Cassio <cassiojr0108@gmail.com>
2023-12-04 14:55:30 -0300

    Create user  entitie

--- modulo3/src/shared/typeorm/index.ts
+++ modulo3/src/shared/typeorm/index.ts
@@ -3,11 +3,12 @@ import { CreateRolesTable1701366349104 } from "./migrations/1701366349104-Create
 import { Role } from "@roles/entities/Role"
 import { CreateUsersTable1701709190656 } from "./migrations/1701709190656-CreateUsersTable"
 import { AddroleIdToUsersTable1701710657971 } from "./migrations/1701710657971-AddroleIdToUsersTable"
+import { User } from "@users/entities/User"
 
 export const dataSource = new DataSource({
     type: 'sqlite',
     database: './db.sqlite',
-    entities: [Role],
+    entities: [Role, User],
     migrations: [
         CreateRolesTable1701366349104, 
         CreateUsersTable1701709190656,
---
+++ modulo3/src/users/entities/User.ts
@@ -0,0 +1,33 @@
+import { Column, CreateDateColumn, Entity, PrimaryColumn } from "typeorm"
+import { v4 as uuidv4 } from "uuid"  
+// Em um contexto de desenvolvimento de backend
+// usando Node.js, o termo “entidades” geralmente
+// se refere a objetos ou modelos que representam
+// os dados do seu aplicativo.
+
+@Entity('users')
+export class User {
+    @PrimaryColumn()
+    id?: string
+
+    @Column()
+    name: string
+
+    @Column()
+    email: string
+
+    @Column()
+    isAdmin: boolean
+
+    @Column()
+    avatar?: string
+
+    @CreateDateColumn()
+    created_at: Date
+
+    constructor() {
+        if(!this.id){
+            this.id = uuidv4()
+        }
+    }
+}
